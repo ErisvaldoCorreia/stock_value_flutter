bd22031031777d3af6d661e4149a5fae62ad0f6b
Erisvaldo_Correia <correiaerisvaldo@gmail.com>
2024-01-10 00:51:21 +0000

    create new pages to control and add initialize supabase keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+      url: 'https://gjyweqtxnaelrfwopopi.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqeXdlcXR4bmFlbHJmd29wb3BpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4NDY3MDksImV4cCI6MjAyMDQyMjcwOX0.U6Run5EuUZJn_zf4SXnQdchOCpIHfEIvz_5NuE8Tw_w');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
